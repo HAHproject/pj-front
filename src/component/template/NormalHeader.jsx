baa62d1cd9345a9362cbabb0096a81736119d187
gotoAbraxas <lty7085@gmail.com>
2023-09-10 02:44:39 +0900

    css 구조적으로 대부분 수정

--- src/component/template/NormalHeader.jsx
+++ src/component/template/NormalHeader.jsx
@@ -1,6 +1,8 @@
 import { Link, useLocation } from "react-router-dom"
 import './NormalHeader.css'
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import AdminMenuBox from "../admin/AdminMenuBox";
 
 
 const NormalHeader = () => {
@@ -12,51 +14,70 @@ const NormalHeader = () => {
     const { scroll } = useSelector(state => state.scrolls)
     console.log('랜더링 테스트')
 
-    const test = () => {
-        console.log('테스트중입니다')
-    }
+
+    const [modal, setModal] = useState(false)
+
+
+
+    // const test = () => {
+    //     console.log('테스트중입니다')
+
+    //     setTest1(true)
+    // }
 
 
 
 
 
-    return <div className="my_header">
-        <div className={`section ${scroll ? 'scroll' : ''}`} >
-            <div className="section-in">
-                <Link to='/main' className={`logo ${scroll ? 'scroll' : ''}`} >저기어때.</Link>
-            </div>
+    return <>
 
-            <div className="section-in">
-                <div className={`fast_menu ${scroll ? 'scroll' : ''}`} >
-                    <div style={{ color: "black" }}>
-                        <Link to="/admin" className="header-button" onMouseEnter={() => test()} >어드민 페이지</Link>
-                    </div>
-                    <div >
-                        <Link to="/admin" className="header-button" >어드민 페이지</Link>
-                    </div>
-                    <div >
-                        <Link to="/admin" className="header-button" >어드민 페이지</Link>
-                    </div>
-                    <div >
-                        <Link to="/admin" className="header-button" >어드민 페이지</Link>
-                    </div>
-                    <div >
-                        <Link to="/admin" className="header-button" >어드민 페이지</Link>
-                    </div>
-                    <div>
-                        <Link to="/owner" className="header-button" >오너 페이지</Link>
+        <div className={`my_header ${scroll ? 'scroll' : ''}`}>
+            <div className="section" >
+                <div className="section_in">
+                    <Link to='/main' className={`logo ${scroll ? 'scroll' : ''}`} >저기어때.</Link>
+                </div>
+
+                <div className="section_in">
+                    <div className={`fast_menu ${scroll ? 'scroll' : ''}`} >
+                        <div onMouseEnter={() => setModal(true)} onMouseLeave={() => setModal(false)} >
+                            <Link to="/admin" className="header-button">어드민 페이지</Link>
+                            <div style={{ position: "absolute" }} >
+                                {modal && <div className="sub_menu"><AdminMenuBox /></div>}
+                            </div>
+
+                        </div>
+                        <div >
+                            <Link to="/admin" className="header-button" >어드민 페이지</Link>
+                        </div>
+                        <div >
+                            <Link to="/admin" className="header-button" >어드민 페이지</Link>
+                        </div>
+                        <div >
+                            <Link to="/admin" className="header-button" >어드민 페이지</Link>
+                        </div>
+                        <div >
+                            <Link to="/admin" className="header-button" >어드민 페이지</Link>
+                        </div>
+                        <div>
+                            <Link to="/owner" className="header-button" >오너 페이지</Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div className="section-2">
-            {firstSegment === 'admin' && <h2 className="more"> 관리자 페이지</h2>}
-            {firstSegment === 'owner' && <h2 className="more"> 영업 페이지</h2>}
+
+        <div className="back_ground">
+            <div className="section-2">
+                {firstSegment === 'admin' && <h2 className="more"> 관리자 페이지</h2>}
+                {firstSegment === 'owner' && <h2 className="more"> 영업 페이지</h2>}
+            </div>
         </div>
 
 
-    </div>
+
+
+    </>
 
 }
 
