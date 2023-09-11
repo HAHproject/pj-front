f46130f566358db9ecd940583baa6ee5690c5760
energysheep <yks7560@gmail.com>
2023-09-12 03:51:57 +0900

    메인 아이콘

---
+++ src/component/main/MainNavIcon.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Motel } from '../../images/motelicon.png';
+import { Hotel } from '../../images/hotelicon.png';
+import { Pansion } from '../../images/pansionicon.png';
+import { Guest } from '../../images/guesticon.png';
+import { Camping } from '../../images/campingicon.png';
+
+
+const MainNavIcon = () => {
+    const navigate = useNavigate();
+    const motelHandle = () => {
+        navigate(``);
+    };
+    const hotelHandle = () => {
+        navigate(``);
+    };
+    const pansionHandle = () => {
+        navigate(``);
+    };
+    const guestHandle = () => {
+        navigate(``);
+    };
+    const campingHandle = () => {
+        navigate(``);
+    };
+
+    return (
+        <IconBox>
+            <Icon>
+                <Motel onClick={motelHandle} />
+                <Text>모텔</Text>
+            </Icon>
+            <Icon>
+                <Hotel onClick={hotelHandle} />
+                <Text>호텔·리조트</Text>
+            </Icon>
+            <Icon>
+                <Pansion onClick={pansionHandle} />
+                <Text>펜션</Text>
+            </Icon>
+            <Icon>
+                <Guest onClick={guestHandle} />
+                <Text>게스트하우스</Text>
+            </Icon>
+            <Icon>
+                <Camping onClick={campingHandle} />
+                <Text>캠핑·글램핑</Text>
+            </Icon>
+        </IconBox>
+    );
+}
+
+export default MainNavIcon;
+
+const IconBox = styled.div`
+  width: 930px;
+  height: 200px;
+  display: flex;
+  box-sizing: border-box;
+  padding: 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  width: 120px;
+  height: 110px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
+const Text = styled.span`
+  font-size: 1.1rem;
+  color: gray;
+`;
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Admin from "../component/admin/Admin"
 import Login from "../component/auth/Login"
 import Signup from "../component/auth/Signup"
+import Main from "../component/main/Main"
 import Template from "../component/template/Template"
 
 
@@ -10,11 +11,12 @@ import Template from "../component/template/Template"
 const MyRoutes = () => {
     return <BrowserRouter>
         <Routes>
+            <Route path="/main" element={<Main />} />
             <Route path="/login" element={<Login></Login>} />
             <Route path="/signup" element={<Signup></Signup>} />
             <Route element={<Template />}>
-                    {/* 템플릿이 적용될 파츠들.. 여기서 고민이 몇가지 있다. */}
-                <Route path="/admin" element={<Admin></Admin>}/>
+                {/* 템플릿이 적용될 파츠들.. 여기서 고민이 몇가지 있다. */}
+                <Route path="/admin" element={<Admin></Admin>} />
 
             </Route>
 
