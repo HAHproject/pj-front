613472aada0a64a4d4a3f9c9ce8047da2e3f1f56
gotoAbraxas <lty7085@gmail.com>
2023-09-15 02:04:17 +0900

    방등록 거의 완료

--- src/component/owner/OwnerRoomPage.jsx
+++ src/component/owner/OwnerRoomPage.jsx
@@ -9,70 +9,49 @@ import './Owner.css'
 import './OwnerRoomPage.css'
 import { api } from "../../network/api/api"
 import uuid from "react-uuid"
+import { async } from "q"
 
 const OwnerRoomPage = () => {
 
-    const sample = data // 업체 정보, 이걸 이제 받아온 상태인것임.
+    console.log('랜더링 테스트 at room')
 
+    const sample = data // 업체 정보, 이걸 이제 받아온 상태인것임.
 
     const nav = useNavigate()
 
     const user = man2
 
-
     const [list, setList] = useState([])
 
-    const test10 = () => {
+    const addRoomHandler = () => {
         const num = uuid()
-        setList([...list, { id: num }])
+        setList([...list, { id: num, flag: false }])
     }
 
-
-
-    const test = { id: '1', roomName: '1' }
-
-    const [data1, setData1] = useState([])
     /**
      * 각자가 함수를 따로 갖아야하는데, 그 함수에서 인덱스를 받아와서
      * 그 인덱스에 맞게 데이터를 만들어서, 던져줘야한다.
      * 
+     * 지금 이 전체가 재 랜더링 되고있음 ...
+     * 
      * 
      */
 
-
-
-
-
-
-
-
-    const test1 = (data) => {
-
-
-        setData1([...data1, data])
-    }
 
     const test2 = () => {
 
         console.log(list)
     }
 
-    const test3 = () => {
 
 
-    }
-
-    const test4 = (id) => {
+    const deleteDataHandler = (id) => {
         setList(list.filter((data) => data.id !== id))
 
     }
 
-
-    const [test100, setTest100] = useState({ id: '', num: '' })
-    // 객체 만들기.
-
-
     const dataSet = (e) => {
+
         const { name, value, id } = e.target
 
         const result = list.map((data) => data.id === id ? { ...data, [name]: value } : data)
@@ -80,33 +59,128 @@ const OwnerRoomPage = () => {
     }
 
 
-    const test13 = (e) => {
-        e.preventDefault()
-        // setData1([...data1, test100])
 
+    const [flag, setFlag] = useState(false)
+
+
+    const save = (e) => {
+        e.preventDefault()
+        const { id } = e.target
+
+        const result = list.map((data) => data.id === id ? { ...data, flag: true } : data)
+        setList(result)
+
+
+
+    }
+
+    const editHandler = (e) => {
+
+        const { id } = e.target
+
+        const result = list.map((data) => data.id === id ? { ...data, flag: false } : data)
+        setList(result)
+
+    }
+
+    const [img, setImg] = useState()
+
+
+    const fileDataSet = async (e) => {
+
+        const selectedFile = e.target.files[0];
+
+        const { name, id } = e.target
+
+        const rId = id.split('/')[1]
+
+
+
+        console.log(name)
+
+        if (selectedFile) {
+            const reader = new FileReader();
+
+            reader.readAsArrayBuffer(selectedFile);
+
+
+
+            reader.onload = (event) => {
+                const fileAsArrayBuffer = event.target.result;
+                // fileAsArrayBuffer를 서버로 전송할 수 있습니다.
+                const file = new Uint8Array(fileAsArrayBuffer);
+
+                const blob = new Blob([file]);
+
+                const url = URL.createObjectURL(blob);
+
+                const result = list.map((data) => data.id === rId ? { ...data, [name]: file, url, imgName: selectedFile.name } : data)
+                setList(result)
+
+
+            };
+
+
+        }
 
     }
 
     const elements = list.map((data) =>
     (
-        <div style={{ display: 'flex', flexDirection: 'column' }} key={data.id}>
-
-            <form onSubmit={(e) => test13(e)}>
-                <input name="test2" id={data.id} placeholder="방정보" onBlur={(e) => dataSet(e)}>
-                </input>
-                <input name="test" id={data.id} placeholder="테스트" onBlur={(e) => dataSet(e)}>
-                </input>
-
-                <input type="file" >
-
-                </input>
-                <button type="submit" > 제출버튼입니다.</button>
-            </form>
-            <div>
-                {data.id}
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', border: '1px solid rgba(0,0,0,0.08)', borderRadius: '3px' }} key={data.id}>
+            <div style={{ padding: '20px' }} >
+                <label htmlFor={`file/${data.id}`}>
+                    {data.url ? <img src={data.url} alt="1"></img> : <img src='https://via.placeholder.com/376x226.png/f4f4f4?text=Click+here+to+upload' alt="1"></img>}
+                </label>
             </div>
 
-            <button onClick={() => test4(data.id)}>삭제 버튼</button>
+            <div >
+                <form id={data.id} onSubmit={(e) => save(e)}>
+
+                    <div style={{ display: 'grid', gridTemplateColumns: '2fr 5fr', gap: '10px' }}>
+                        <span>
+                            방정보
+                        </span>
+                        <div>
+                            <input name="roomName" type="text" id={data.id} placeholder="방정보" onBlur={(e) => dataSet(e)} disabled={data.flag} />
+                        </div>
+                        <span>
+                            가격
+                        </span>
+                        <div>
+                            <input name="price" type="number" id={data.id} placeholder="가격" onBlur={(e) => dataSet(e)} disabled={data.flag} />
+                        </div>
+
+                        <span>
+                            이미지
+                        </span>
+                        <div>
+                            <input type="file" style={{ display: 'none' }} id={`file/${data.id}`} name="file" onChange={(e) => fileDataSet(e)} disabled={data.flag} />
+                            <label htmlFor={`file/${data.id}`}>
+                                <div className="room_img_item">
+                                    <span className="room_btn-upload">업로드</span>
+                                    {data.imgName ? <div>{data.imgName}</div> : <div>파일을 올려주세요</div>}
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+
+                    {/* <div style={{ margin: '30px 0 0 0' }}>
+                        {!data.flag && <button type="submit" > 저장하기</button>}
+                        {data.flag && <button id={data.id} onClick={(e) => editHandler(e)}>수정하기</button>}
+                    </div> */}
+                    <div style={{ display: 'flex', justifyContent: 'space-between', margin: '30px 0 0 0' }}>
+                        {!data.flag && <button type="submit" > 저장하기</button>}
+                        {data.flag && <button id={data.id} onClick={(e) => editHandler(e)}>수정하기</button>}
+                        <button onClick={() => deleteDataHandler(data.id)}>삭제</button>
+                    </div>
+
+                </form>
+
+            </div>
+
+
+
 
         </div>
     ))
@@ -155,7 +229,7 @@ const OwnerRoomPage = () => {
 
 
     return <>
-        <div style={{ display: "flex", flexDirection: 'column', gap: '10px' }}>
+        <div style={{ display: "flex", flexDirection: 'column', gap: '14px' }}>
             <div className="owner_status">
                 <OwnerIndexDetail user={user} />
             </div>
@@ -178,11 +252,11 @@ const OwnerRoomPage = () => {
                 </div>
             </div>
 
-            <button onClick={() => test1()}>test 1</button>
-
             {elements}
-
-            <button onClick={() => test10()}> 추가하기</button>
+            <div style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center' }}>
+                <div style={{ margin: '0 0 0 20px' }}>✔️ 반드시 저장하지 않아도 됩니다.</div>
+                <button onClick={() => addRoomHandler()}> 방 추가하기</button>
+            </div>
             <button onClick={() => test2()}>test 2</button>
             {/* 유즈 스테이트에 0:{객체} 1:{객체} 이런식으로 저장하고 싶음 [data...[index]:객체] */}
 
